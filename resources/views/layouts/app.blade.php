b5f6f6772813cb8a2e43c58c6a51731e6740c2f1
adityaeks <mohadityaekosaputra11@gmail.com>
2025-01-04 20:19:52 +0700

    Layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html class="h-100" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+    <meta name="description" content="A growing collection of ready to use components for the CSS framework Bootstrap 5">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon-16x16.png') }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('img/favicon.png') }}">
+    <meta name="author" content="Holger Koenemann">
+    <meta name="generator" content="Eleventy v2.0.0">
+    <meta name="HandheldFriendly" content="true">
+    <title>Stride HTML Template - Frontpage one</title>
+    <link rel="stylesheet" href="{{ asset('css/theme.min.css') }}">
+
+    <style>
+        /* inter-300 - latin */
+        @font-face {
+            font-family: 'Inter';
+            font-style: normal;
+            font-weight: 300;
+            font-display: swap;
+            src: local(''),
+                url('{{ asset('fonts/inter-v12-latin-300.woff2') }}') format('woff2'),
+                url('{{ asset('fonts/inter-v12-latin-300.woff') }}') format('woff');
+        }
+
+        @font-face {
+            font-family: 'Inter';
+            font-style: normal;
+            font-weight: 500;
+            font-display: swap;
+            src: local(''),
+                url('{{ asset('fonts/inter-v12-latin-500.woff2') }}') format('woff2'),
+                url('{{ asset('fonts/inter-v12-latin-500.woff') }}') format('woff');
+        }
+
+        @font-face {
+            font-family: 'Inter';
+            font-style: normal;
+            font-weight: 700;
+            font-display: swap;
+            src: local(''),
+                url('{{ asset('fonts/inter-v12-latin-700.woff2') }}') format('woff2'),
+                url('{{ asset('fonts/inter-v12-latin-700.woff') }}') format('woff');
+        }
+    </style>
+</head>
+
+<body data-bs-spy="scroll" data-bs-target="#navScroll">
+    @include('layouts.nav')
+    @yield('content')
+
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/aos.js') }}"></script>
+    <script>
+        AOS.init({
+            duration: 800, // values from 0 to 3000, with step 50ms
+        });
+    </script>
+
+    <script>
+        let scrollpos = window.scrollY
+        const header = document.querySelector(".navbar")
+        const header_height = header.offsetHeight
+
+        const add_class_on_scroll = () => header.classList.add("scrolled", "shadow-sm")
+        const remove_class_on_scroll = () => header.classList.remove("scrolled", "shadow-sm")
+
+        window.addEventListener('scroll', function() {
+            scrollpos = window.scrollY;
+
+            if (scrollpos >= header_height) {
+                add_class_on_scroll()
+            } else {
+                remove_class_on_scroll()
+            }
+
+            console.log(scrollpos)
+        })
+    </script>
+
+</body>
+
+</html>
